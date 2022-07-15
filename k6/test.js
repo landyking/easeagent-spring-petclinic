fc12ae7291b365e530c3f4d6955a14ce8643ea77
landyking <landykingdom@gmail.com>
2022-07-15 13:35:22 +0800

    polish

--- k6/test.js
+++ k6/test.js
@@ -1,6 +1,14 @@
 import http from "k6/http";
 import { sleep, check } from "k6";
 
+export const options = {
+  stages: [
+    { duration: "30s", target: 100 },
+    { duration: "5m", target: 100 },
+    // { duration: "2m", target: 0 },
+  ],
+};
+
 export default function () {
   const BASE_URL = "http://localhost:8080"; // make sure this is not production
 
